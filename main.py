a0e53f5a1d239e55906d29f12282635e37007c6b
mandiner <kleemandarsen@gmail.com>
2020-12-02 20:12:20 +0100

    Create Advent 2020 day 2

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from advent_2018_day_2.handler import check_sum_counter, find_common_words
 from advent_2018_day_3.handler import overlap_checker, overlap_checker_2
 from advent_2020_day_1.handler import find_two_values_to_sum_2020_and_multiplying,\
     find_three_values_to_sum_2020_and_multiplying
-
+from advent_2020_day_2.handler import password_validator, super_password_validator
 
 if __name__ == '__main__':
     # Start Timer
     start_time = dt.datetime.now()
 
     # Parsing
-    raw_data = input_parser('advent_2018_day_3/input.txt')
+    raw_data = input_parser('advent_2020_day_2/input.txt')
     data = formatter(raw_data)
 
     # Run
-    result = overlap_checker_2(data)
+    result = super_password_validator(data)
 
     # End of Task
     print(f"Run time: {dt.datetime.now() - start_time}")
