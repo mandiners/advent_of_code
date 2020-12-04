81de251c5092ffa865a4943c5062f99c9f87009a
mandiner <kleemandarsen@gmail.com>
2020-12-04 20:20:33 +0100

    Upgrade Advent 2020 day 4

--- advent_2020_day_4/handler.py
+++ advent_2020_day_4/handler.py
@@ -16,17 +16,19 @@ def passport_validator(lines):
     228
     """
     valid_passports = 0
-    fields = []
+    fields = set()
+    lines = lines if lines[-1] is None else [*lines, *[None]]
+
     for line in lines:
         if line:
             data = line.split(' ')
             for field in data:
-                fields.append(field.split(':')[0])
+                fields.add(field.split(':')[0])
         else:
             fields = [i for i in fields if i != 'cid']
-            if len(REQUIRED_FIELDS) == len(fields):
+            if sorted(fields) == sorted(REQUIRED_FIELDS):
                 valid_passports += 1
-            fields = []
+            fields = set()
 
     return valid_passports
 
@@ -35,27 +37,28 @@ def passport_validator2(lines):
     """
     175
     """
-    threshold = len(REQUIRED_FIELDS)
     valid_passports = 0
     data = {}
+    lines = lines if lines[-1] is None else [*lines, *[None]]
 
     for line in lines:
         if line:
-            line = line.split(' ')
-            for field in line:
+            fields = line.split(' ')
+            for field in fields:
                 key, value = field.split(':')
-                data[key] = value
+                if key != 'cid':
+                    data[key] = value
         else:
-            valid_fields = 0
-            for key, value in data.items():
-                if key == 'cid':
-                    continue
-                if validator := getattr(validators, f'{key}_validator'):
-                    is_valid = validator(value)
-                    if is_valid:
-                        valid_fields += 1
-            if valid_fields == threshold:
+            for field_name, value in data.items():
+                if validator := getattr(validators, f'{field_name}_validator'):
+                    data[field_name] = validator(value)
+
+            field_list = set(i for i in data.keys())
+            are_all_fields_are_valid = all(value for value in data.values())
+
+            if are_all_fields_are_valid and sorted(field_list) == sorted(REQUIRED_FIELDS):
                 valid_passports += 1
+
             data = {}
 
     return valid_passports
