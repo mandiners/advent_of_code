e5884d7fba6d13e41a7a4a839c44d7a2a648b713
mandiner <kleemandarsen@gmail.com>
2020-12-04 17:52:30 +0100

    Create Advent 2020 day 3

---
+++ advent_2020_day_3/handler.py
@@ -0,0 +1,44 @@
+def tree_counter(lines):
+    """
+    198
+    """
+    results = 0
+    position = 3
+    for index, line in enumerate(lines):
+        if index != 0:
+            if position > len(line):
+                while len(line) < position:
+                    line += line
+            if line[position] == '#':
+                results += 1
+            position += 3
+
+    return results
+
+
+def tree_counter2(lines):
+    """
+    5140884672
+    """
+    slopes = ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
+    tree_list = []
+    for slope in slopes:
+        result = 0
+        right, down = slope
+        position = right
+        for index, line in enumerate(lines):
+            if index != 0 and index % down == 0:
+                if position >= len(line):
+                    while len(line) <= position:
+                        line += line
+                if line[position] == '#':
+                    result += 1
+                position += right
+
+        tree_list.append(result)
+
+    results = 1
+    for i in tree_list:
+        results = results * i
+
+    return results
--- main.py
+++ main.py
@@ -6,17 +6,18 @@ from advent_2018_day_3.handler import overlap_checker, overlap_checker_2
 from advent_2020_day_1.handler import find_two_values_to_sum_2020_and_multiplying,\
     find_three_values_to_sum_2020_and_multiplying
 from advent_2020_day_2.handler import password_validator, super_password_validator
+from advent_2020_day_3.handler import tree_counter, tree_counter2
 
 if __name__ == '__main__':
     # Start Timer
     start_time = dt.datetime.now()
 
     # Parsing
-    raw_data = input_parser('advent_2020_day_2/input.txt')
+    raw_data = input_parser('advent_2020_day_3/input.txt')
     data = formatter(raw_data)
 
     # Run
-    result = super_password_validator(data)
+    result = tree_counter2(data)
 
     # End of Task
     print(f"Run time: {dt.datetime.now() - start_time}")
