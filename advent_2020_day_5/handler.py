1c9f3f6e0563f93593cc5a9b07dc449fe06415f7
mandiner <kleemandarsen@gmail.com>
2020-12-05 15:52:43 +0100

    Create Advent 2020 day 5

--- advent_2020_day_5/handler.py
+++ advent_2020_day_5/handler.py
@@ -27,6 +27,6 @@ def seat_locator(input_lines):
     """
     seat_id_list = get_seat_ids(input_lines)
 
-    for seat in range(min(seat_id_list) - 1, max(seat_id_list) + 1):
+    for seat in range(min(seat_id_list), max(seat_id_list)):
         if seat not in seat_id_list:
             return seat
