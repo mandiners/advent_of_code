43852425c6b10d63d910b6d78685284beba8ad5e
mandiner <kleemandarsen@gmail.com>
2020-12-05 16:22:51 +0100

    Update Advent 2020 day 5

--- advent_2020_day_5/handler.py
+++ advent_2020_day_5/handler.py
@@ -1,14 +1,21 @@
+def convert_bin_to_dec(data, one, zero):
+    return int(data.replace(one, '1').replace(zero, '0'), 2)
+
+
 def get_seat_ids(lines):
+    """
+    Line example: 'FBFBBFFRLR'.
+    Row is the first 7 characters from line: 'FBFBBFF' => 0101100 => 44.
+    Column is the last 3 characters from line: 'RLR' => 101 => 5.
+    Seat id is 8 * row + column = > 8 * 44 + 5 = 357
+    """
     seat_ids = []
 
     for line in lines:
-        row = line[0:7]
-        column = line[7::]
+        row = convert_bin_to_dec(data=line[0:7], one='B', zero='F')
+        column = convert_bin_to_dec(data=line[7::], one='R', zero='L')
 
-        row = int(''.join('0' if r == 'F' else '1' for r in row), 2)
-        column = int(''.join('0' if c == 'L' else '1' for c in column), 2)
         seat_id = (8 * row) + column
-
         seat_ids.append(seat_id)
 
     return seat_ids
