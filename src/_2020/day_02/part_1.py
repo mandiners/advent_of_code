f2e467cd6c0ce73aaea9e7080127ebe044682d80
mandiner <kleemandarsen@gmail.com>
2020-12-05 21:14:22 +0100

    Refactor 2020 day_02

--- src/_2020/day_02/part_1.py
+++ src/_2020/day_02/part_1.py
@@ -2,17 +2,19 @@ def run(input_data):
     """
     393
     """
-    results = 0
+    result = 0
     for data in input_data:
-        policy_rules, password = [i for i in data.split(': ')]
-        policy_range, policy_target = policy_rules.split(' ')
-        policy_range = policy_range.split('-')
         temp = 0
+
+        policy_rules, password = data.split(': ')
+        policy_range, policy_target = policy_rules.split(' ')
+        start, end = [int(i) for i in policy_range.split('-')]
+
         for char in password:
             if policy_target == char:
                 temp += 1
 
-        if temp in range(int(policy_range[0]), int(policy_range[1]) + 1):
-            results += 1
+        if temp in range(start, end + 1):
+            result += 1
 
-    return results
+    return result
--- src/_2020/day_02/part_2.py
+++ src/_2020/day_02/part_2.py
@@ -2,17 +2,15 @@ def run(input_data):
     """
     690
     """
-    results = 0
+    result = 0
     for data in input_data:
-        policy_rules, password = [i for i in data.split(': ')]
+        policy_rules, password = data.split(': ')
         policy_range, policy_target = policy_rules.split(' ')
-        first, second = [int(i) - 1 for i in policy_range.split('-')]
-        temp = 0
-        for char in (password[first], password[second]):
-            if char == policy_target:
-                temp += 1
+        first_index, second_index = [int(i) - 1 for i in policy_range.split('-')]
 
-        if temp == 1:
-            results += 1
+        target_characters = {password[first_index], password[second_index]}
 
-    return results
+        if policy_target in target_characters and len(target_characters) == 2:
+            result += 1
+
+    return result
