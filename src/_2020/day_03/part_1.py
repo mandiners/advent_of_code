7eb587e4bf2d06babaf23375b7b67895f5543e98
mandiner <kleemandarsen@gmail.com>
2021-01-25 15:40:01 +0100

    refactor

--- src/_2020/day_03/part_1.py
+++ src/_2020/day_03/part_1.py
@@ -1,16 +1,16 @@
+from . import THREE
+
+
 def run(lines):
     """
     198
     """
     results = 0
     position = 3
-    for index, line in enumerate(lines):
-        if index != 0:
-            if position > len(line):
-                while len(line) < position:
-                    line += line
-            if line[position] == '#':
-                results += 1
-            position += 3
+    for line in lines[1::]:
+        index = position % len(line)
+        if line[index] == THREE:
+            results += 1
+        position += 3
 
     return results
--- src/_2020/day_03/part_2.py
+++ src/_2020/day_03/part_2.py
@@ -1,3 +1,4 @@
+from . import THREE
 import functools
 
 
@@ -11,12 +12,10 @@ def run(lines):
         result = 0
         right, down = slope
         position = right
-        for index, line in enumerate(lines):
-            if index != 0 and index % down == 0:
-                if position >= len(line):
-                    while len(line) <= position:
-                        line += line
-                if line[position] == '#':
+        for index, line in enumerate(lines[::down]):
+            if index != 0:
+                current_pos = position % len(line)
+                if line[current_pos] == THREE:
                     result += 1
                 position += right
 
